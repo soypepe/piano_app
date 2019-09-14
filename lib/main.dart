ed7247a838a0f30869606d51d21340e9b2598135
Pepe Alandia Gonzales <juan_j_19@hotmail.com>
2019-09-14 15:49:35 -0400

    Agregado mensaje de salida cuando se apreta el boton Atras

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'componentes/slider.dart';
@@ -5,14 +7,73 @@ import 'componentes/slider.dart';
 void main() {
   setOrientacion();
   hideStatusBar();
-  runApp(Piano());
+  runApp(Principal());
 }
 
-class Piano extends StatelessWidget {
+class Principal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: Piano(),
+    );
+  }
+}
+
+class Piano extends StatefulWidget {
+  @override
+  _PianoState createState() => _PianoState();
+}
+
+class _PianoState extends State<Piano> {
+  Future<bool> _atrasPresionado() {
+    return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Center(
+              child: Text('¿Cerrar la aplicacion?'),
+            ),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.green[300],
+                  child: Text(
+                    'Quedarse',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () => Navigator.pop(context, false),
+                ),
+                FlatButton(
+                  color: Colors.red[400],
+                  child: Text(
+                    'Salir',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                    exit(0);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ) ??
+        false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _atrasPresionado,
+      child: Scaffold(
         backgroundColor: Colors.black38,
         body: SafeArea(
           child: SliderPiano(),
